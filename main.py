0ce5fb4afd90f149506bca3599afd5ef5d8b624d
arist0v <verret.martin@gmail.com>
2022-01-28 19:08:34 -0500

    pasted main.py from other project

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+"""Hydro-Quebec winter power saving event"""
+
+from os import path
+import functools
+import signal
 import sys
+import time
+
+sys.path.append(path.join(path.dirname(path.abspath(__file__)), 'lib'))#TODO: Figure this out
+
+#from pkg.tplink_adapter import TPLinkAdapter  # TODO adapt this line to fit my need
+
+
+_DEBUG = False
+_ADAPTER = None
+
+print = functools.partial(print, flush=True)#TODO: figure out this
+
+
+def cleanup(signum, frame):
+    """Clean up any resources before exiting."""
+    if _ADAPTER is not None:
+        _ADAPTER.close_proxy()
+
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGINT, cleanup)
+    signal.signal(signal.SIGTERM, cleanup)
+    #_ADAPTER = TPLinkAdapter(verbose=_DEBUG)#TODO: adapt this line to fit my need
+
+    # Wait until the proxy stops running, indicating that the gateway shut us
+    # down.
+    while _ADAPTER.proxy_running():
+        time.sleep(2)
