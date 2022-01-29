5d5a52716c6d7269bb512a2a653f9c3025c8622b
arist0v <verret.martin@gmail.com>
2022-01-29 14:45:38 -0500

    created default property object

--- pkg/hq_Property.py
+++ pkg/hq_Property.py
@@ -2,3 +2,18 @@
 
 from gateway_addon import Property
 
+class hqProperty(Property):
+    """Property type for HQdata"""
+    
+    def __init__(self, device, name, description, value):
+        """
+        Initialize the object
+        
+        device -- the DEvice this property belongs to
+        name -- name of the property
+        description -- description of the property
+        value -- current falue of this property
+        """
+        Property.__init__(device, name, description)
+        self.set_cached_value(value)
+
