def3dc23f4bdb2aa9a75b66985d8913333c6be1b
arist0v <verret.martin@gmail.com>
2022-01-28 20:43:30 -0500

    added debug line in main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def cleanup(signum, frame):
 
 
 if __name__ == '__main__':
+    print("crocrodile")
     signal.signal(signal.SIGINT, cleanup)
     signal.signal(signal.SIGTERM, cleanup)
     _ADAPTER = hqAdapter()
