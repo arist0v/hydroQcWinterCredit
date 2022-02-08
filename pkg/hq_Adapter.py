c8a6f2ac232a06bb2dffcb50addbe8dae43fba8f
arist0v <verret.martin@gmail.com>
2022-02-08 11:49:56 -0500

    debuging loaddbconfig

--- pkg/hq_Adapter.py
+++ pkg/hq_Adapter.py
@@ -60,15 +60,13 @@ class hqAdapter(Adapter):
 
         Return the config object as dict
         """
-        database = Database(package_name)
+        database = Database('hydroQcWinterCredit')#TODO: edited for debug
         if not database.open():
+            print("Can't open database")
             return
         config = database.load_config()
         database.close()
         print(config)
-        if not config:
-            print("Can't load data from config inside")
-            return
 
         return config
 
