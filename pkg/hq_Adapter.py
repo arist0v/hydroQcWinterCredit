aa68226d8ac2f7aea706f1aeae57f9d5aa7146d7
arist0v <verret.martin@gmail.com>
2022-02-07 20:00:47 -0500

    add 2 methods to handle database config

--- pkg/hq_Adapter.py
+++ pkg/hq_Adapter.py
@@ -14,12 +14,12 @@ class hqAdapter(Adapter):
         """initialize the object"""
         self.name = self.__class__.__name__
         Adapter.__init__(self,'hydroQcWinterCredit','hydroQcWinterCredit')#argument: self, id for the package, name of the package
-        """
-        database = Database('hydroQcWinterCredit')#creating database objet with the id as argument
-        database.open()#opening database
-        print(database.load_config())#DEBUG Test Reading database for config #Loading config
-        database.close()
-        """ #TEST FOR DATA BASE READ
+        config = self.load_db_config(self.get_id())
+
+        if not config:
+            print("Can't load config from Database")
+            return
+
         self.pairing=False
         self.start_pairing(_TIMEOUT)
 
@@ -41,10 +41,40 @@ class hqAdapter(Adapter):
     def remove_device(self, device):
         """Removing the device from webthings"""
 
-        self.handle_device_removed(device)
+        #self.handle_device_removed(device)
         #TODO: Error in log: ERROR  : Error getting thing description for thing with id hqDevice12345: Error: Unable to find thing with id: hqDevice12345
         #  at /home/node/webthings/gateway/build/webpack:/src/models/things.js:268:1 TO BE FIXED LATER
 
     def cancel_pairing(self):
         """Cancel the pairing process"""
         self.pairing = False
+
+    def load_db_config(self, package_name):
+        """
+        Load configuration from DB
+
+        package_name -- name of the package as shown in the manifest.json
+
+        Return the config object as dict
+        """
+        database = Database[package_name]
+        if not database.open():
+            return
+        config = database.load_config()
+        database.close()
+
+        if not config:
+            return
+
+        return config
+
+    def save_config_to_db(self, package_name):
+        """
+        save configuration to DB
+
+        package_name -- name of the package as shown in manifest.json
+        """
+        database = Database[package_name]
+        database.open()
+        database.save_config()
+        database.close()
